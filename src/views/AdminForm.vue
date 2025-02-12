c2dc9b6de9e8208bf7a69949989d68a171ad638a
KhunSiThu <khunsithuaung65@gmail.com>
2025-02-12 08:42:08 +0630

    dark

--- src/views/AdminForm.vue
+++ src/views/AdminForm.vue
@@ -1,50 +1,49 @@
 <template>
-    <Loading v-if="clickSubmit"></Loading>
+    <div :class="isDarkMode ? 'dark' : 'light'" class="w-screen h-screen flex justify-center items-center">
 
-    <div class="w-screen h-screen flex justify-center items-center">
-        <div class="w-full md:w-[360px] p-8 md:p-0">
-            <!-- Login Form -->
-            <div class="mb-6">
-                <h2 class="text-xl font-semibold text-gray-900 dark:text-white mb-10">Login to Admin Account</h2>
-                <form @submit.prevent="handleLogin">
-                    <!-- Admin Name Input -->
-                    <div class="relative z-0 w-full mb-5 group">
-                        <input type="text" v-model="adminName" id="login_adminName"
-                            class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                            placeholder=" " required />
-                        <label for="login_adminName"
-                            class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
-                            Admin Name
-                        </label>
-                    </div>
-
-                    <!-- Password Input -->
-                    <div class="relative z-0 w-full mb-5 group">
-                        <i :class="passwordVisible ? 'fa-regular fa-eye-slash' : 'fa-regular fa-eye'"
-                            class="absolute right-0 p-2.5 cursor-pointer"
-                            @click="togglePasswordVisibility"></i>
-                        <input :type="passwordVisible ? 'text' : 'password'" v-model="password" id="login_password"
-                            class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                            placeholder=" " required />
-                        <label for="login_password"
-                            class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
-                            Password
-                        </label>
-                    </div>
-
-                    <p v-if="errorMessage" class="text-red-600 text-sm prose text-justify mb-6">{{ errorMessage }}</p>
-
-                    <div class="mt-10 text-center">
-                        <button type="submit"
-                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            Login
-                        </button>
-                    </div>
-                </form>
+  
+      <div class="w-full md:w-[360px] p-8 md:p-0">
+        <!-- Login Form -->
+        <div class="mb-6">
+          <h2 class="text-xl font-semibold text-gray-900 dark:text-white mb-10">Login to Admin Account</h2>
+          <form @submit.prevent="handleLogin">
+            <!-- Admin Name Input -->
+            <div class="relative z-0 w-full mb-5 group">
+              <input type="text" v-model="adminName" id="login_adminName"
+                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" " required />
+              <label for="login_adminName"
+                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+                Admin Name
+              </label>
             </div>
+  
+            <!-- Password Input -->
+            <div class="relative z-0 w-full mb-5 group">
+              <i :class="passwordVisible ? 'fa-regular fa-eye-slash' : 'fa-regular fa-eye'"
+                class="absolute right-0 p-2.5 cursor-pointer" @click="togglePasswordVisibility"></i>
+              <input :type="passwordVisible ? 'text' : 'password'" v-model="password" id="login_password"
+                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" " required />
+              <label for="login_password"
+                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+                Password
+              </label>
+            </div>
+  
+            <p v-if="errorMessage" class="text-red-600 text-sm prose text-justify mb-6">{{ errorMessage }}</p>
+  
+            <div class="mt-10 text-center">
+              <button type="submit"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Login
+              </button>
+            </div>
+          </form>
         </div>
+      </div>
     </div>
-</template>
+  </template>
 
 <script>
 import adminLogin from "@/composables/adminLogin";
